bb00a0ad79ddd524e38511cc3ccc8cbd96d2b035
Cornelius Alfredo <33976366+c0rz@users.noreply.github.com>
2021-03-30 10:08:15 +0700

    update sederhana

--- source-code/application/controllers/Welcome.php
+++ source-code/application/controllers/Welcome.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +21,10 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		if (!$this->session->userdata('credentials')) :
+			$this->load->view('welcome_message');
+		else :
+			var_dump("Session di terima, Tampilan dalam tahap pembuatan");
+		endif;
 	}
 }
