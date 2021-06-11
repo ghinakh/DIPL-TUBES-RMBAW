a52bdeac952da10f65effb1797a3efbc987dd585
Cornelius Alfredo <33976366+c0rz@users.noreply.github.com>
2021-06-11 15:05:26 +0700

    fixxxx

--- source-code/application/controllers/Payment.php
+++ source-code/application/controllers/Payment.php
@@ -30,18 +30,20 @@ class Payment extends CI_Controller
     {
         if (!$this->session->userdata('credentials')) :
         else :
-            $data['web_config'] = $this->Database->getData("konfigurasi_web", array('id' => 1));
-            $ses = $this->session->userdata('credentials');
-            $data['user'] =  $ses[0];
-            $n = explode(' ', $ses[0]["nama_lengkap"]);
-            $data["nama_dipisah"] = $n;
-            $foto = '';
-            for ($x = 0; $x <= 1; $x++) {
-                $foto .= substr($n[$x], 0, 1);
-            }
-            $data["foto_profile"] = $foto;
-            $this->load->view('include/head', $data);
-            $this->load->view('page/detail', $data);
+            $id  = $this->input->post('id_mobil');
+            var_dump($id);
+        // $data['web_config'] = $this->Database->getData("konfigurasi_web", array('id' => 1));
+        // $ses = $this->session->userdata('credentials');
+        // $data['user'] =  $ses[0];
+        // $n = explode(' ', $ses[0]["nama_lengkap"]);
+        // $data["nama_dipisah"] = $n;
+        // $foto = '';
+        // for ($x = 0; $x <= 1; $x++) {
+        //     $foto .= substr($n[$x], 0, 1);
+        // }
+        // $data["foto_profile"] = $foto;
+        // $this->load->view('include/head', $data);
+        // $this->load->view('page/detail', $data);
         endif;
     }
 }
